10c26d3ee45d89982dad3c98188f0c137ed4c7bc
Tony Lofreso Jr <tonylofresojr@gmail.com>
2019-12-01 21:40:02 -0500

    added skeleton for endpoints.py

--- ise/endpoints.py
+++ ise/endpoints.py
@@ -1,4 +1,22 @@
 from ise import exceptions
 
 class Endpoints(object):
-    pass
+    def __init__(self, ise_con):
+        self.ise_con = ise_con
+
+    def get_groups(self, **kwargs):
+        # Return all identity groups
+        pass
+
+    def get_endpoints(self, **kwargs):
+        # Return all endpoints
+        pass
+
+    def create_endpoint():
+        pass
+
+    def update_endpoint():
+        pass
+
+    def delete_endpoint():
+        pass
